09a6e6526f76a78dab4a830df345d1ee0122a53c
mzpolini <matthew.polini@gmail.com>
2015-11-16 05:39:38 -0500

    initial express commit

--- app.js
+++ app.js
@@ -1,62 +1,60 @@
-// 'use strict';
-// var bitcore;
-// var request;
-// angular.module('oats')
-//
-// .run( function run () {
-//   bitcore = require('bitcore-lib');
-//   request = require('request');
-// })
-//
-// .controller('MyCtrl', ['$scope', 'request',
-//  function ($scope, request) {
-//     function IssueAsset() {
-//       var bcToken = '4eb1e48b0c88b8d3220adf101289c743';
-//
-//       createNewAddress();
-//       //create new address with token
-//
-//       //fund new address, for testnet on blockcypher we will
-//       //use the faucet
-//
-//
-//       //using now funded address, issue # amount of assets
-//     };
-//
-//     function createNewAddress() {
-//     request(
-//         { method: 'POST'
-//         , uri: 'https://api.blockcypher.com/v1/btc/test3/addrs'
-//         }
-//       , function (error, response, body) {
-//           if(response.statusCode == 201){
-//             var info = JSON.parse(body);
-//             console.log("PK :" + info.private);
-//             console.log("public :" + info.public);
-//             console.log("address :" + info.address);
-//             console.log("wif :" + info.wif);
-//           } else {
-//             console.log('error: '+ response.statusCode)
-//             console.log(body)
-//             console.alert("PK :" + info.private);
-//           }
-//         }
-//       )
-//     };
-//
-//     $scope.cancel = function(){
-//       // Returns app to zero-state
-//       delete $scope.files;
-//       $scope.stampSuccess = false;
-//       $scope.previousTimestamps = [];
-//       $scope.pendingTimestamp = null;
-//       $scope.cancelStamp();
-//     };
-//
-//     $scope.openTransactionInBrowser = function(transactionId){
-//       require('shell').openExternal('https://test-insight.bitpay.com/tx/' + transactionId);
-//     };
-//
-//
-//
-// }]);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
